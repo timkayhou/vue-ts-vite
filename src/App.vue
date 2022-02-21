7b531e0db57c937a76267a06e5d583ab0b6624b3
Expurgator <timkayhou@gmail.com>
2022-02-21 22:55:15 +0900

    ✨ feat: json

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import json from '../public/app.json';
 import './assets/styles/app.less';
 import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/images/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite + Less + ESLint + SFC" />
+  <HelloWorld :msg="json.msg" />
 </template>
