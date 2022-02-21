394edb03b7021e02d4f8c7272ba8c88d9397c0ec
Expurgator <timkayhou@gmail.com>
2022-02-21 16:18:20 +0900

    🎈 perf: remove packged file name hash code

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,9 @@ export default defineConfig({
       inlineDynamicImports: true,
       output: {
         manualChunks: () => "everything.js",
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
       },
     },
   }
